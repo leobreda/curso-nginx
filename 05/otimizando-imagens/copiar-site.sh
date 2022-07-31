02725d1a5fcf8b734d5a44d48b9aaba76b0cf070
Leonardo Breda <leobreda@yahoo.com>
2022-07-30 21:52:52 -0300

    05

--- 05/otimizando-imagens/copiar-site.sh
+++ 05/otimizando-imagens/copiar-site.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo Copiando o site...
-cp -r /home/curso/site-demo-1/site /home/curso/5.otimizando-imagens/site
+cp -r /home/curso/site-demo-1/site /home/curso/05/otimizando-imagens/site
 
 echo Criando pasta logs...
 mkdir logs
