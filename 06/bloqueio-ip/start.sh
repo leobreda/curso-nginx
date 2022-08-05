2f6963e3f0e4022141b562a5bd18eb5fe57b6ad0
Leonardo Breda <leobreda@yahoo.com>
2022-08-04 23:02:18 -0300

    organizando pastas

---
+++ 06/bloqueio-ip/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo Copiando o site...
+cp -r /home/curso/extras/site-demo-php /home/curso/06/bloqueio-ip/site
+
+#echo Adicionando biblioteca nginx-extras
+#apt install nginx-extras -y
+
+echo Criando pasta logs...
+mkdir logs
