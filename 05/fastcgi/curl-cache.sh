0a4a40799f9d6dacbd6e5482323f9da0f665d753
Leonardo Breda <leobreda@yahoo.com>
2022-08-07 10:50:10 -0300

    trocando ip por etc/hosts

--- 05/fastcgi/curl-cache.sh
+++ 05/fastcgi/curl-cache.sh
@@ -2,7 +2,7 @@
 
 
 echo Medindo o tempo do site...
-curl -I -w"\nTime: %{time_total}\n" http://192.168.1.13:1002/delay5seg.php
+curl -I -w"\nTime: %{time_total}\n" http://127.0.0.1:1002/delay5seg.php
 
 echo Medindo novamente o tempo do site...
-curl -I -w"\nTime: %{time_total}\n" http://192.168.1.13:1002/delay5seg.php
+curl -I -w"\nTime: %{time_total}\n" http://127.0.0.1:1002/delay5seg.php
