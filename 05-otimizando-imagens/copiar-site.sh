fd1b2348db682abfeaf131a2698c93b4cf762567
Leonardo Breda <leobreda@yahoo.com>
2023-02-12 20:34:10 -0300

    05-otimizando-imagens html

--- 05-otimizando-imagens/copiar-site.sh
+++ 05-otimizando-imagens/copiar-site.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo Copiando o site...
-cp -r /home/curso/extras/site-demo-php/site /home/curso/05-otimizando-imagens/site
+cp -r /home/curso/extras/site-demo-html /home/curso/05-otimizando-imagens/site
 
 echo Criando pasta logs...
 mkdir logs
