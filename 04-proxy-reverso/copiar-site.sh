60284c20d743fe643a72e85c6f6e941d5b4e22c7
Leonardo Breda <leobreda@yahoo.com>
2023-02-12 19:16:42 -0300

    04-proxy-reverso

---
+++ 04-proxy-reverso/copiar-site.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo Copiando o site...
+cp -r /home/curso/extras/site-demo-php /home/curso/04-proxy-reverso/site
+
+echo Criando pasta logs...
+mkdir /home/curso/04-proxy-reverso/logs
+
