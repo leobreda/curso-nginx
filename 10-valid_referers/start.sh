2a68aea36456958bf0f33c183d3a18653fea35ab
Leonardo Breda <leobreda@yahoo.com>
2023-02-21 19:53:24 -0300

    10-valid_referers

--- 10-valid_referers/start.sh
+++ 10-valid_referers/start.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+Encerrando processo na porta 1001
+kill -9 $(lsof -t -i:1001)
+
+
 echo Copiando o site...
-cp -r /home/curso/extras/site-demo-php /home/curso/06/valid_referers/site
+cp -r /home/curso/extras/site-demo-php /home/curso/10-valid_referers/site
 
 echo Adicionando biblioteca nginx-extras
 apt install nginx-extras -y
