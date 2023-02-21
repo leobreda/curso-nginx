c1ebba5e9a9487dfcf3bb56cba1a8ff492631f9e
Leonardo Breda <leobreda@yahoo.com>
2023-02-21 17:53:25 -0300

    07-rewrite-rule

--- 07-rewrite-rules/copiar-site.sh
+++ 07-rewrite-rules/copiar-site.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo Copiando o site...
-cp -r /home/curso/extras/site-demo-php /home/curso/05/rewrite-rules/site
+cp -r /home/curso/extras/site-demo-php /home/curso/07-rewrite-rules/site
 
 echo Criando pasta logs...
 mkdir logs
