19de44946410e49fced774edb2df677b25a37d52
Leonardo Breda <leobreda@yahoo.com>
2023-02-21 23:10:02 -0300

    13-error_page

--- 13-error_page/start.sh
+++ 13-error_page/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo Copiando o site...
-cp -r /home/curso/extras/site-demo-php /home/curso/06/error_page/site
+cp -r /home/curso/extras/site-demo-php /home/curso/13-error_page/site
 
 echo Criando pasta logs...
 mkdir logs
