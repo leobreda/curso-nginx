f17020bf2c1fb1b55f87406296d5cff0f6cc78d0
Leonardo Breda <leobreda@yahoo.com>
2023-02-22 09:43:13 -0300

    14-load-balance

--- etc/14-https/gera-certificados.sh
+++ etc/14-https/gera-certificados.sh
@@ -5,4 +5,4 @@ timedatectl set-timezone America/Sao_Paulo
 
 echo GERANDO CERTIFICADO
 openssl genrsa -out ubuntu.key 2048
-openssl req -new -x509 -key ubuntu.key -out ubuntu.crt -nodes -days 3650  -subj '/C=BR/ST=SP/L=Sao Paulo/O=Meu Site/CN=ubuntu'
+openssl req -new -x509 -key ubuntu.key -out ubuntu.crt -nodes -days 365  -subj '/C=BR/ST=SP/L=Sao Paulo/O=Meu Site/CN=ubuntu'
