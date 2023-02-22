2621ecaf6b801fc0a5e5fa2b96369ba54373ea6a
Leonardo Breda <leobreda@yahoo.com>
2023-02-21 22:09:51 -0300

    11-bloqueio-ip

--- 11-bloqueio-ip/start.sh
+++ 11-bloqueio-ip/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo Copiando o site...
-cp -r /home/curso/extras/site-demo-php /home/curso/06/bloqueio-ip/site
+cp -r /home/curso/extras/site-demo-php /home/curso/11-bloqueio-ip/site
 
 #echo Adicionando biblioteca nginx-extras
 #apt install nginx-extras -y
